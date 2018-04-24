581023a96a5a5e0a09f609e39db648a7dd71a2be
Sam Glerum <samuelglerum@hotmail.com>
2018-04-24 15:10:06 +0200

    added recipes to api_v2 routes. Added get-handler for getting recipes

--- MyFirstMochaProject/routes/routes_api_v2.js
+++ MyFirstMochaProject/routes/routes_api_v2.js
@@ -2,12 +2,18 @@
 let express = require('express');
 let router = express.Router();
 let path = require('path');
+let recipes = require('.././recipes');
 
 router.get('/info', (req,res) => {
     res.status(200);
     res.json({
         "description": "You have reached the V2 Mocha testing server"
     });
+});
+
+router.get('/recipes', (req,res) => {
+   res.status(200);
+   res.json(recipes);
 });
 
 router.get('*', (req, res) => {
